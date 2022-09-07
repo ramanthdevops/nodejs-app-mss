23930def29131e13dacb2d466b32b2bf0f2025fa
venkataRamanth <107137201+venkataRamanth@users.noreply.github.com>
2022-09-07 22:20:39 +0530

    Update sonar-project.js
    updated sonar qube public ip and token on 07-09-2022

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.246.209:9000/',
+  serverUrl: 'http://15.206.173.197:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '007fa4025c11328682eea2bf88c1479976633faf',
+	    'sonar.login': '629e21b070a1039550422c78c89eb492701bfa4b',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
